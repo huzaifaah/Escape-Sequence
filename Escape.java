1608a71020011890dc8b59b8ad25ff8ee4e23ab4
huzaifa <107106387+huzaifaah@users.noreply.github.com>
2022-06-12 14:03:27 +0500

    HuzaifaAfzal

---
+++ Escape.java
@@ -0,0 +1,13 @@
+public class Escape
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hey\nHi");
+		System.out.println("One\rTwo");
+		System.out.println("A\bB");
+		System.out.println("Huzaifa\tIs\tNaughty");
+		System.out.println("\"Huzaifa\"");
+		System.out.println("\'Virus\'");
+		System.out.println("\\Khaad\tFactory\\");
+	}
+}
